f9c526466f8f154ef46facd6db0f16a92de88429
Alex Singh <alex@alexsingh.com>
2019-07-29 20:06:23 -0300

    Updates font-size, weight, color and font-family of components and adds hover state to Link

--- src/styled/components/index.js
+++ src/styled/components/index.js
@@ -3,7 +3,6 @@ import { BACKGROUND_LIGHT } from '../themes'
 
 export const Header = styled.div`
   background-color: ${BACKGROUND_LIGHT};
-  min-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -13,22 +12,26 @@ export const Header = styled.div`
 `
 
 export const Title = styled.h1`
-  color: #d677ed;
+  color: #2659ff;
   font-size: 16pt;
   margin-bottom: 60px;
-  font-family: 'Avenir';
-  font-weight: 200;
+  font-family: 'system-ui', Helvetica Neue, sans-serif;
+  font-weight: 900;
   letter-space: 0.5pt;
-  font-size 1.25rem
+  font-size 1.5rem;
+  justify-self: center;
 `
 
 export const Link = styled.a`
   color: #e85223;
   text-decoration: none;
-  font-size: 12pt;
-  font-family: 'Avenir';
+  font-family: 'system-ui', Helvetica Neue, sans-serif;
   letter-spacing: 0.5pt;
   cursor: pointer;
+
+  :hover {
+    color: #2659ff;
+  }
 `
 
 export const AlignItemsRow = styled.div`
