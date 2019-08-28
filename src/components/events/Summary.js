2c488b787ef0a761cd0ec76a5aeead69a952efb9
Zach Sherman <zach@shrmn.toys>
2019-08-28 13:37:06 -0400

    group event log by resource

--- src/components/events/Summary.js
+++ src/components/events/Summary.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import Octicon from 'react-octicon'
 import dayjs from 'dayjs'
-import { Event } from '../events/Event'
+import { Event, EventObjectContainer } from '../events/Event'
 import {
   PUSH_EVENT,
   ISSUE_COMMENT_EVENT,
@@ -12,15 +12,22 @@ import {
 } from '../../constants'
 import { switchCases } from '../../utils'
 import { EventListWrapper } from './EventList'
+import { BLUE_TRANSP, BLUE, BR_LILAC } from '../../styled/themes'
 
 const TimeSummary = ({ summary, isExpanded }) => {
   const resources = sortResources(summary.resources)
   return (
-    <SummaryContainer>
-      {resources.map(resource => (
-        <ResourceSummary resource={resource} isExpanded={isExpanded} />
-      ))}
-    </SummaryContainer>
+    <EventListWrapper>
+      {resources.map(resource =>
+        isExpanded ? (
+          <ResourceEventLog resource={resource} />
+        ) : (
+          <SummaryContainer>
+            <ResourceSummary resource={resource} />
+          </SummaryContainer>
+        )
+      )}
+    </EventListWrapper>
   )
 }
 
@@ -29,7 +36,85 @@ TimeSummary.propTypes = {
   isExpanded: PropTypes.bool.isRequired
 }
 
-const ResourceSummary = ({ resource, isExpanded }) => {
+const ResourceEventLog = ({ resource }) => {
+  const [openClosedOrMerged, setOCM] = useState(null)
+
+  useEffect(() => {
+    let localIsOpen = null
+    let alreadyChecked = false
+
+    const routeEvents = {
+      [ISSUE_COMMENT_EVENT]: event => {
+        if (!alreadyChecked) {
+          event.data.payload.issue.state === 'open'
+            ? (localIsOpen = 'Open')
+            : (localIsOpen = 'Closed')
+          alreadyChecked = true
+        }
+      },
+      [ISSUES_EVENT]: event => {
+        if (!alreadyChecked) {
+          event.data.payload.issue.state === 'open'
+            ? (localIsOpen = 'Open')
+            : (localIsOpen = 'Closed')
+          alreadyChecked = true
+        }
+      },
+      [PULL_REQUEST_EVENT]: event => {
+        if (!alreadyChecked) {
+          if (event.data.payload.action === 'closed') {
+            event.data.payload.pull_request.merged
+              ? (localIsOpen = 'Merged')
+              : (localIsOpen = 'Closed')
+          } else if (event.data.payload.action === 'opened')
+            localIsOpen = 'Open'
+
+          alreadyChecked = true
+        }
+      }
+    }
+    const getSummaryOfEvents = switchCases(routeEvents)(() => {})
+
+    const sortedEvents = sortResourceEvents(resource.events)
+    sortedEvents.forEach(event => {
+      getSummaryOfEvents(event.type)(event)
+    })
+
+    setOCM(localIsOpen)
+  }, [resource, resource.events])
+
+  return (
+    <EventListWrapper>
+      <SummaryHeader>
+        <Type>
+          <TypeIcon
+            type={resource.type}
+            openClosedOrMerged={openClosedOrMerged}
+          />
+        </Type>
+        <Title>
+          {resource.title.indexOf('refs/head') > -1
+            ? resource.title.substr(11, resource.title.length)
+            : resource.title}
+        </Title>
+      </SummaryHeader>
+      {resource.events.map(event => (
+        <Event
+          key={event.id}
+          createdAt={event.createdAt}
+          data={event.data}
+          type={event.type}
+          user={event.user}
+        />
+      ))}
+    </EventListWrapper>
+  )
+}
+ResourceEventLog.propTypes = {
+  resource: PropTypes.object.isRequired
+}
+
+const ResourceSummary = ({ resource }) => {
   const [commentCount, setCommentCount] = useState(0)
   const [commitCount, setCommitCount] = useState(0)
   const [openClosedOrMerged, setOCM] = useState(null)
@@ -87,34 +172,7 @@ const ResourceSummary = ({ resource, isExpanded }) => {
   }, [resource, resource.events])
 
   if (resource.type === 'branches' && openClosedOrMerged !== null) return null
-  if (isExpanded) {
-    return (
-      <EventListWrapper>
-        <Summary>
-          <Type>
-            <TypeIcon
-              type={resource.type}
-              openClosedOrMerged={openClosedOrMerged}
-            />
-          </Type>
-          <Title>
-            {resource.title.indexOf('refs/head') > -1
-              ? resource.title.substr(11, resource.title.length)
-              : resource.title}
-          </Title>
-        </Summary>
-        {resource.events.map(event => (
-          <Event
-            key={event.id}
-            createdAt={event.createdAt}
-            data={event.data}
-            type={event.type}
-            user={event.user}
-          />
-        ))}
-      </EventListWrapper>
-    )
-  }
+
   return (
     <Summary>
       <Type>
@@ -145,6 +203,9 @@ const ResourceSummary = ({ resource, isExpanded }) => {
       </Summaries>
     </Summary>
   )
+}
+ResourceSummary.propTypes = {
+  resource: PropTypes.object.isRequired
 }
 
 const TypeIcon = ({ type, openClosedOrMerged }) => {
@@ -182,11 +243,6 @@ TypeIcon.defaultProps = {
   openClosedOrMerged: null
 }
 
-ResourceSummary.propTypes = {
-  resource: PropTypes.object.isRequired,
-  isExpanded: PropTypes.bool.isRequired
-}
-
 const sortResourceEvents = events => {
   return events.sort((eventA, eventB) =>
     dayjs(eventA.createdAt).isAfter(dayjs(eventB.createdAt)) ? -1 : 1
@@ -203,8 +259,13 @@ const sortResources = resources => {
 export const SummaryContainer = styled.div`
   position: relative;
   max-width: 960px;
+  width: 100%;
+  background: ${BR_LILAC};
   margin: 0.875rem;
   padding: 0.875rem;
+  border-radius: 4px;
+  border: solid 1px ${BLUE_TRANSP};
+  box-shadow: -3px 3px ${BLUE};
   font-size: 1rem;
   font-family: 'Lucida Console', Monaco, monospace;
   cursor: pointer;
@@ -229,6 +290,15 @@ const Title = styled.div`
 const Summaries = styled.div`
   position: relative;
   left: 140px;
+`
+
+const SummaryHeader = styled(EventObjectContainer)`
+  border: none;
+  background: none;
+  box-shadow: none;
+  margin-top: 0.875rem;
+  font-size: 1rem;
+  font-family: 'Lucida Console', Monaco, monospace;
 `
 
 export default TimeSummary
