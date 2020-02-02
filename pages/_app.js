b8cc4f8da8f1d3cc61a7e1cca141ce21dae61797
Alex Singh <alex@alexsingh.com>
2020-02-02 17:13:33 -0300

    Beginning to build out page with comonents

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,43 @@ import App from 'next/app'
 import React from 'react'
 import withReduxStore from '../lib/with-redux-store'
 import { Provider } from 'react-redux'
+import styled from 'styled-components'
+import { ThemeProvider } from 'styled-components'
+
+import { Box, Menu, MenuItem } from '../components/index.js'
+
+const StreamMenu = styled(Menu)({
+})
+
+const StreamObject = styled(MenuItem)({
+})
 
 class MyApp extends App {
   render() {
     const { Component, pageProps, reduxStore } = this.props
     return (
       <Provider store={reduxStore}>
-        <Component {...pageProps} />
+      <Box></Box>
+        {/*<Component {...pageProps} />*/}
+
+        <Box
+        display={'block'}
+        minHeight={'100vh'}
+        p={3}
+        >
+        <StreamMenu
+        display={'inline'}
+        py={2}
+        >
+        <StreamObject
+        display={'inline-block'}
+        background={'#000'}
+        px={3}
+        py={2}
+        >Stream</StreamObject>
+        </StreamMenu>
+        </Box>
+
       </Provider>
     )
   }
