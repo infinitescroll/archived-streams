890aff9accfa950e35ca7d951a6f93e062b6f78b
Alex Singh <alex@alexsingh.com>
2020-02-05 08:53:54 -0300

    Refactored index components to conform to styled-components rendering conventions

--- components/index.js
+++ components/index.js
@@ -1,46 +1,44 @@
 import styled from 'styled-components'
 import { color, typography, border, layout, position, space, flexbox, grid } from 'styled-system'
 
-export const Box = styled.div(
+export const Box = styled.div`
   {
-    boxSizing: 'border-box',
-    minWidth: 0,
-  },
-  space,
-  color,
-  layout,
-  flexbox
-)
+    list-style: 'border-box';
+    list-style: 0;
+  }
+  ${space}
+  ${color}
+  ${layout}
+  ${flexbox}
+`
 
-export const Menu = styled.ul(
+export const Menu = styled.ul`
   {
-    listStyle: 'none',
-  },
-  space,
-  color,
-  layout,
-  flexbox
-)
+    list-style: none;
+  }
+  ${space}
+  ${color}
+  ${layout}
+  ${flexbox}
+`
 
-export const MenuItem = styled.li(
-  {
-    listStyle: 'none',
-  },
-  space,
-  color,
-  layout,
-  flexbox
-)
+export const MenuItem = styled.li`
+  ${space}
+  ${color}
+  ${layout}
+  ${flexbox}
+`
 
 
-export const Icon = styled.span({
-  space,
-  color,
-  layout,
-  flexbox
-})
+export const Icon = styled.span`
+  ${space}
+  ${color}
+  ${layout}
+  ${flexbox}
+  ${border}
+`
 
-export const Text = styled.p({
-  color,
-  typography
-})
+export const Text = styled.p`
+  ${color}
+  ${typography}
+`
