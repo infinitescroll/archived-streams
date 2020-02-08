fa3d638ca530c659dff761a852b39f2c39633348
Alex Singh <alex@alexsingh.com>
2020-02-08 16:10:50 -0300

    Updates StreamEditor styling to improve responsiveness

--- components/StreamEditor.jsx
+++ components/StreamEditor.jsx
@@ -3,17 +3,19 @@ import { Box, Text, Menu, MenuItem } from '.'
 import Button from './Button'
 
 const StreamEditorRow = styled(Menu)`
-  display: inline;
+  display: inline-block;
+  margin: 0;
   li {
-    display: inline;
-    margin: ${props => props.theme.space[2]}px;
+    display: inline-block;
+    margin: ${props => props.theme.space[1]}px
+      ${props => props.theme.space[2]}px;
   }
 `
 
 export default () => (
   <Box p={3} border="1px solid" borderColor="lightGrey">
     <StreamEditorRow p={2}>
-      <MenuItem>
+      <MenuItem width={1}>
         <Text display="inline">Show Me</Text>
       </MenuItem>
       <MenuItem>
@@ -29,14 +31,56 @@ export default () => (
         <Button>A weekly summary</Button>
       </MenuItem>
     </StreamEditorRow>
-    <Box p={2}>
-      <Text>Of</Text>
-    </Box>
-    <Box p={2}>
-      <Text>By</Text>
-    </Box>
-    <Box p={2}>
-      <Text>On Branch</Text>
-    </Box>
+    <StreamEditorRow p={2}>
+      <MenuItem width={1}>
+        <Text display="inline">Of</Text>
+      </MenuItem>
+      <MenuItem>
+        <Button>All Activity</Button>
+      </MenuItem>
+      <MenuItem>
+        <Button>An hourly summary</Button>
+      </MenuItem>
+      <MenuItem>
+        <Button>A daily summary</Button>
+      </MenuItem>
+      <MenuItem>
+        <Button>A weekly summary</Button>
+      </MenuItem>
+    </StreamEditorRow>
+    <StreamEditorRow p={2}>
+      <MenuItem width={1}>
+        <Text display="inline">By</Text>
+      </MenuItem>
+      <MenuItem>
+        <Button>All Activity</Button>
+      </MenuItem>
+      <MenuItem>
+        <Button>An hourly summary</Button>
+      </MenuItem>
+      <MenuItem>
+        <Button>A daily summary</Button>
+      </MenuItem>
+      <MenuItem>
+        <Button>A weekly summary</Button>
+      </MenuItem>
+    </StreamEditorRow>
+    <StreamEditorRow p={2}>
+      <MenuItem width={1}>
+        <Text display="inline">On Branch</Text>
+      </MenuItem>
+      <MenuItem>
+        <Button>All Activity</Button>
+      </MenuItem>
+      <MenuItem>
+        <Button>An hourly summary</Button>
+      </MenuItem>
+      <MenuItem>
+        <Button>A daily summary</Button>
+      </MenuItem>
+      <MenuItem>
+        <Button>A weekly summary</Button>
+      </MenuItem>
+    </StreamEditorRow>
   </Box>
 )
