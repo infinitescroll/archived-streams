a0dcc2299faf04ade2e4636e081c332d3ad96af7
Alex Singh <alex@alexsingh.com>
2020-02-08 22:00:52 -0300

    Onboarding step 3 initial commit

--- pages/onboard.jsx
+++ pages/onboard.jsx
@@ -1,8 +1,9 @@
 import styled from 'styled-components'
-import { Box, Text } from '../components/index'
+import { Box, Text, Menu, MenuItem } from '../components/index'
 import Input from '../components/Input'
 import Button from '../components/Button'
 import { IcoGitHub } from '../components/icons'
+import StreamEditor from '../components/StreamEditor'
 
 const PageWrapper = styled(Box)``
 
@@ -28,7 +29,7 @@ export default () => (
           <Text>First, give your Stream a name.</Text>
         </Box>
         <Box>
-          <Input></Input>
+          <Input />
         </Box>
         <Box textAlign="center">
           <Button>Cancel</Button>
@@ -62,6 +63,62 @@ export default () => (
             </Box>
             Connect to GitHub
           </Button>
+        </Box>
+        <Box textAlign="center">
+          <Button>Cancel</Button>
+        </Box>
+      </Box>
+    </PageWrapper>
+    <PageWrapper
+      display="flex"
+      justifyContent="center"
+      alignContent="center"
+      p={3}
+      height="100vh"
+    >
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="space-around"
+        height="100%"
+        flexGrow="1"
+        maxWidth="480px"
+      >
+        <Box textAlign="center">
+          <h1>Step 3</h1>
+          <Text>Lastly, let's add a repo or two to your new stream</Text>
+        </Box>
+        <Box textAlign="left">
+          <Input />
+          <Menu>
+            <MenuItem display="block">
+              <Text>You own</Text>
+            </MenuItem>
+            <MenuItem display="inline-block" mr={3} my={2}>
+              <Button>Hangtime</Button>
+            </MenuItem>
+            <MenuItem display="inline-block" mr={3} my={2}>
+              <Button>Streams</Button>
+            </MenuItem>
+            <MenuItem display="inline-block" mr={3} my={2}>
+              <Button>Filament</Button>
+            </MenuItem>
+          </Menu>
+          <Menu>
+            <MenuItem display="block">
+              <Text>You're collaborating on</Text>
+            </MenuItem>
+            <MenuItem display="inline-block" mr={3} my={2}>
+              <Button>Hangtime</Button>
+            </MenuItem>
+            <MenuItem display="inline-block" mr={3} my={2}>
+              <Button>Streams</Button>
+            </MenuItem>
+            <MenuItem display="inline-block" mr={3} my={2}>
+              <Button>Filament</Button>
+            </MenuItem>
+          </Menu>
+          <StreamEditor />
         </Box>
         <Box textAlign="center">
           <Button>Cancel</Button>
