47e9fedf39f86cc369d97b550fe5a6787948cb33
Alex Singh <alex@alexsingh.com>
2020-02-08 21:40:53 -0300

    Onboarding step 2 initial commit

--- pages/onboard.jsx
+++ pages/onboard.jsx
@@ -2,35 +2,71 @@ import styled from 'styled-components'
 import { Box, Text } from '../components/index'
 import Input from '../components/Input'
 import Button from '../components/Button'
+import { IcoGitHub } from '../components/icons'
 
 const PageWrapper = styled(Box)``
 
 export default () => (
-  <PageWrapper
-    display="flex"
-    justifyContent="center"
-    alignContent="center"
-    p={3}
-    height="100vh"
-  >
-    <Box
+  <>
+    <PageWrapper
       display="flex"
-      flexDirection="column"
-      justifyContent="space-around"
-      height="100%"
-      flexGrow="1"
-      maxWidth="480px"
+      justifyContent="center"
+      alignContent="center"
+      p={3}
+      height="100vh"
     >
-      <Box textAlign="center">
-        <h1>Step 1</h1>
-        <Text>First, give your Stream a name.</Text>
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="space-around"
+        height="100%"
+        flexGrow="1"
+        maxWidth="480px"
+      >
+        <Box textAlign="center">
+          <h1>Step 1</h1>
+          <Text>First, give your Stream a name.</Text>
+        </Box>
+        <Box>
+          <Input></Input>
+        </Box>
+        <Box textAlign="center">
+          <Button>Cancel</Button>
+        </Box>
       </Box>
-      <Box>
-        <Input></Input>
+    </PageWrapper>
+    <PageWrapper
+      display="flex"
+      justifyContent="center"
+      alignContent="center"
+      p={3}
+      height="100vh"
+    >
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="space-around"
+        height="100%"
+        flexGrow="1"
+        maxWidth="480px"
+      >
+        <Box textAlign="center">
+          <h1>Step 2</h1>
+          <Text>Next, let's connect to GitHub</Text>
+          <Text>You’ll have to authorize Streams to access your repo data</Text>
+        </Box>
+        <Box textAlign="center">
+          <Button primary>
+            <Box width="32px" display="inline-block">
+              <IcoGitHub maxWidth="48px" opacity="0.4" />
+            </Box>
+            Connect to GitHub
+          </Button>
+        </Box>
+        <Box textAlign="center">
+          <Button>Cancel</Button>
+        </Box>
       </Box>
-      <Box textAlign="center">
-        <Button>Cancel</Button>
-      </Box>
-    </Box>
-  </PageWrapper>
+    </PageWrapper>
+  </>
 )
