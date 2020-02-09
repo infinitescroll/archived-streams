4a56f4b180b6f101d98476412ae3ece86b71f38e
Alex Singh <alex@alexsingh.com>
2020-02-08 21:24:09 -0300

    Initial commit.

--- pages/onboard.jsx
+++ pages/onboard.jsx
@@ -1,1 +1,36 @@
-export default () => <div>Yo</div>
+import styled from 'styled-components'
+import { Box, Text } from '../components/index'
+import Input from '../components/Input'
+import Button from '../components/Button'
+
+const PageWrapper = styled(Box)``
+
+export default () => (
+  <PageWrapper
+    display="flex"
+    justifyContent="center"
+    alignContent="center"
+    p={3}
+    height="100vh"
+  >
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="space-around"
+      height="100%"
+      flexGrow="1"
+      maxWidth="480px"
+    >
+      <Box textAlign="center">
+        <h1>Step 1</h1>
+        <Text>First, give your Stream a name.</Text>
+      </Box>
+      <Box>
+        <Input></Input>
+      </Box>
+      <Box textAlign="center">
+        <Button>Cancel</Button>
+      </Box>
+    </Box>
+  </PageWrapper>
+)
